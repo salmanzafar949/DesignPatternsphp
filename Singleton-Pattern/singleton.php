48148ce88b86166612754d0c4d77e0588bca0a12
Salman Zafar <salmanzafar949@gmail.com>
2017-11-20 14:15:47 +0500

    Singleton pattern implementation Completed

--- Singleton-Pattern/singleton.php
+++ Singleton-Pattern/singleton.php
@@ -9,3 +9,43 @@
 // Signleton design pattern
 // it allows user have to one and only instance of a class in the project/script
 // it is considered to be the granddadday of all the design patterns
+// Singleton is nothin more than a fancy name for global variable..
+
+class DbConn{
+
+    public static $instance;
+    protected $server = "localhost";
+    protected $uname  = "root";
+    protected $pass   = "";
+    protected $db     = "lrs";
+    public    $conn   = "";
+
+    private function __construct()
+    {
+        $this->connect($this->server, $this->uname,$this->pass,$this->db);
+    }
+
+    public static function get_instance()
+    {
+        if(!isset(DbConn::$instance))
+        DbConn::$instance = new DbConn();
+        return DbConn::$instance;
+    }
+
+    public function connect($server, $user,$pass, $db)
+    {
+        $this->conn = new mysqli($server,$user,$pass,$db);
+        if($this->conn->connect_error)
+        {
+            die("Connection failed: ".$this->conn->connect_error);
+        }
+    }
+}
+
+$db = DbConn::get_instance();
+//$db2 = DbConn::get_instance();
+//$db3 = DbConn::get_instance();
+//echo "<pre>";
+//var_dump($db);
+//var_dump($db2);
+//var_dump($db3);
