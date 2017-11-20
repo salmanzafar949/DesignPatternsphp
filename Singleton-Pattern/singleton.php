9edd32854726df5261d6ad2b7038384f067eee50
Salman Zafar <salmanzafar949@gmail.com>
2017-11-20 13:49:57 +0500

    Deisgn Patterns

---
+++ Singleton-Pattern/singleton.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Salman zafar.
+ * User: sam
+ * Date: 11/20/17
+ * Time: 1:48 PM
+ */
+
+// Signleton design pattern
+// it allows user have to one and only instance of a class in the project/script
+// it is considered to be the granddadday of all the design patterns
